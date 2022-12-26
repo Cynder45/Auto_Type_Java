9bb81e59c4196425ca4c4d7261662393508a4921
Cynder45 <104248326+Cynder45@users.noreply.github.com>
2022-12-26 10:00:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".autotyped", {
+    strings: [ "Terima Kasih", "Terima Gaji", "Gak Ada yang mau nanya ?", "Yakin?", "Oke deh"],
+    typeSpeed: 100,
+    backSpeed: 150,
+    loop: true,
+});
